69c74121df0ef3b0dc8c3157f1fe07d10de420a4
Jad Tala <Jad.Tala@protonmail.com>
2022-02-16 11:47:53 +0100

    (Commands) Play command update

---
+++ src/commands/music/Play.ts
@@ -0,0 +1,20 @@
+import { BaseCommandInteraction, Client } from 'discord.js'
+import * as yt from 'youtube-search-without-api-key';
+
+import { Command } from '../../Command'
+import { play } from '../functions'
+
+export const Play: Command = {
+    name: 'play',
+    description: 'Plays the requested song to your voice channel.',
+    options: [{ name: 'query', description: 'The keywords for the requested song', type: 'STRING', required: true }],
+    type: 'CHAT_INPUT',
+    run: async (client: Client, interaction: BaseCommandInteraction) => {
+        const query = interaction.options.get('query')
+        if (!query) return
+        
+        const result = (await yt.search(`${query.value}`))[0]
+
+        play(client, interaction, result)
+    }
+}
