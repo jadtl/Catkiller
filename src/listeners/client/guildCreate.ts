d22e7c24a5cb8d95a9f9d3d582506e329fbc6973
Jad Tala <Jad.Tala@protonmail.com>
2022-02-16 11:49:27 +0100

    (Listeners) Client server join listener

---
+++ src/listeners/client/guildCreate.ts
@@ -0,0 +1,20 @@
+import { Client, Guild } from 'discord.js'
+import * as Discord from '@discordjs/voice'
+
+import { Queue } from '../../data/Queue'
+import { timeout, queue, player, nowPlaying } from '../../commands/state'
+import error from '../audio/error'
+import idle from '../audio/idle'
+
+export default (client: Client): void => {
+    client.on('guildCreate', async (guild: Guild) => {
+        const id = parseInt(guild.id)
+        //update global variables for newly joined guild
+        timeout[id] = undefined
+        queue[id] = new Queue()
+        player[id] = Discord.createAudioPlayer()
+        error(player[id])
+        idle(player[id], id)
+        nowPlaying[id] = undefined
+    })
+}
