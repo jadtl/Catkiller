22f34f8c64a5a83b5a3bfadbeae9b73f14c17edd
Jad Tala <Jad.Tala@protonmail.com>
2022-02-16 11:47:27 +0100

    (Commands) Now playing command

---
+++ src/commands/music/NowPlaying.ts
@@ -0,0 +1,20 @@
+import { BaseCommandInteraction, Client } from 'discord.js'
+import { Command } from '../../Command'
+import { nowPlaying } from '../state'
+
+export const NowPlaying: Command = {
+    name: 'nowplaying',
+    description: 'Tells what song is currently playing.',
+    type: 'CHAT_INPUT',
+    run: async (client: Client, interaction: BaseCommandInteraction) => {
+        if (!interaction.guild) return
+        const id = parseInt(interaction.guild.id)
+
+        const result = nowPlaying[id]
+        if (!result) {
+            interaction.editReply({ content: `No song is currently playing.` })
+        } else {
+            interaction.editReply({ content: `Now playing **${result.title}**.` })
+        }
+    }
+}
