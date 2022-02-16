ef7c584942e2524ddcacec6dc26dee6b0b81b063
Jad Tala <Jad.Tala@protonmail.com>
2022-02-16 11:48:01 +0100

    (Commands) Queue command

---
+++ src/commands/music/Queue.ts
@@ -0,0 +1,25 @@
+import { BaseCommandInteraction, Client } from 'discord.js'
+
+import { Command } from '../../Command'
+import { nowPlaying, player, queue } from '../state'
+
+export const Queue: Command = {
+    name: 'queue',
+    description: 'Displays the songs\' queue.',
+    type: 'CHAT_INPUT',
+    run: async (client: Client, interaction: BaseCommandInteraction) => {
+        if (!interaction.guild) return
+        const id = parseInt(interaction.guild.id)
+        
+        if (queue[id].isEmpty) {
+            interaction.editReply({ content: 'There are no songs in the queue.' })
+            return
+        }
+        const list = queue[id].list()
+
+        interaction.editReply({ content: `**Songs queue**:\n${list
+            .map(song => `**${list.indexOf(song) + 1}**. ${song.title} \`${song.duration}\`.`)
+            .join('\n')}`
+        })
+    }
+}
